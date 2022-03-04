7d57a83deccafdab70cbcac97c9a7230b2b6e1e5
Robin Schroer <git@sulami.xyz>
2022-03-04 13:38:02 +0100

    Read from a Serial TTY in a loop and print to stdout

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
-fn main() {
-    println!("Hello, world!");
+use std::io::prelude::*;
+use std::io::{self, stdout, Write};
+use std::path::Path;
+
+use serial_core::{SerialDevice, SerialPortSettings};
+use serial_unix::{TTYPort, TTYSettings};
+
+fn main() -> io::Result<()> {
+    let mut tty = TTYPort::open(Path::new("/dev/tty.usbmodemTEST1"))?;
+    tty.set_timeout(std::time::Duration::new(5, 0))?;
+    let mut settings: TTYSettings = tty.read_settings()?;
+    settings.set_baud_rate(serial_core::Baud9600)?;
+    tty.write_settings(&settings)?;
+    let mut char_buf = [0u8];
+    loop {
+        tty.read_exact(&mut char_buf)?;
+        print!("{}", String::from_utf8(char_buf.to_vec()).unwrap());
+        stdout().flush()?;
+    }
+
+    // write!(tty, "Hey there.")?;
 }
