9637457b120f828fb3d456d02014ecf154b826ab
Robin Schroer <git@sulami.xyz>
2022-03-07 17:12:38 +0100

    Improve ergonomics by handling temporary disconnects
    If a write to the device fails, just fall back into a loop and keep
    retrying to open the device. This means a single session can be kept
    open if the physical device gets unplugged and plugged back in, or
    otherwise severs the serial connection.

--- src/main.rs
+++ src/main.rs
@@ -42,62 +42,62 @@ struct Args {
 fn main() -> io::Result<()> {
     let args = Args::parse();
 
-    let mut tty = TTYPort::open(&args.path)?;
-    let _ = tty.reconfigure(&|settings| {
-        settings.set_baud_rate(args.baud).unwrap();
-        Ok(())
-    });
+    'outer: loop {
+        if let Ok(mut tty) = TTYPort::open(&args.path) {
+            let _ = tty.reconfigure(&|settings| {
+                settings.set_baud_rate(args.baud).unwrap();
+                Ok(())
+            });
 
-    let tty_arc = Arc::new(Mutex::new(tty));
+            let tty_arc = Arc::new(Mutex::new(tty));
 
-    let output_arc = Arc::clone(&tty_arc);
-    thread::spawn(move || {
-        output_handler(output_arc);
-    });
+            let output_arc = Arc::clone(&tty_arc);
+            thread::spawn(move || {
+                output_handler(output_arc);
+            });
 
-    let input_arc = Arc::clone(&tty_arc);
-    let input = thread::spawn(move || {
-        input_handler(input_arc);
-    });
-
-    input.join().unwrap();
-
-    Ok(())
-}
-
-fn input_handler(tty_mutex: Arc<Mutex<TTYPort>>) {
-    loop {
-        print!("> ");
-        stdout().flush().unwrap();
-        let mut buf = String::new();
-        stdin().read_line(&mut buf).unwrap();
-        match buf.as_str() {
-            "/quit\n" => {
-                println!("Bye!");
-                return;
+            let input_arc = Arc::clone(&tty_arc);
+            loop {
+                print!("> ");
+                stdout().flush().unwrap();
+                let mut buf = String::new();
+                stdin().read_line(&mut buf).unwrap();
+                match buf.as_str() {
+                    "/quit\n" => {
+                        println!("Bye!");
+                        return Ok(());
+                    }
+                    _ => {
+                        if let Ok(mut tty) = input_arc.lock() {
+                            let write_result = write!(tty, "{}", buf);
+                            drop(tty);
+                            if write_result.is_err() {
+                                continue 'outer;
+                            }
+                            // Wait a bit here to avoid looping back up and
+                            // writing our prompt into output that's still being
+                            // printed. Because we dropped the TTY, the mutex is
+                            // released and the output handler can do its thing.
+                            thread::sleep(Duration::from_millis(200));
+                        }
+                    }
+                }
             }
-            _ => {
-                let mut tty = tty_mutex.lock().unwrap();
-                write!(tty, "{}", buf).unwrap();
-                drop(tty);
-                // Wait a bit here to avoid looping back up and
-                // writing our prompt into output that's still being
-                // printed. Because we dropped the TTY, the mutex is
-                // released and the output handler can do its thing.
-                thread::sleep(Duration::from_millis(10));
-            }
+        } else {
+            // Failed to open TTY.
+            println!("Device offline, waiting...");
+            thread::sleep(Duration::from_secs(5));
+            continue;
         }
     }
 }
 
-fn output_handler(tty_mutex: Arc<Mutex<TTYPort>>) -> ! {
-    let mut char_buf = [0u8];
+fn output_handler(tty_mutex: Arc<Mutex<TTYPort>>) {
     loop {
-        // At 9600 Baud we get a new byte every 105 micros. Sleep a
-        // while to give other threads a chance to capture the TTY.
         thread::sleep(Duration::from_micros(100));
+        let mut char_buf = [0u8; 255];
         let mut tty = tty_mutex.lock().unwrap();
-        if tty.read_exact(&mut char_buf).is_ok() {
+        if tty.read(&mut char_buf).is_ok() {
             print!("{}", String::from_utf8(char_buf.to_vec()).unwrap());
             stdout().flush().unwrap();
         }
